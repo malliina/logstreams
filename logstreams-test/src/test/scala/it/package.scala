0742f48a4167060fe05a30f099d6829361576a9f
Michael Skogberg <michael.skogberg@futurice.com>
2018-04-08 23:29:42 +0300

    Increase timeout

--- logstreams-test/src/test/scala/it/package.scala
+++ logstreams-test/src/test/scala/it/package.scala
@@ -2,5 +2,5 @@ import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 package object it {
-  def await[T](f: Future[T]): T = Await.result(f, 5.seconds)
+  def await[T](f: Future[T]): T = Await.result(f, 100.seconds)
 }

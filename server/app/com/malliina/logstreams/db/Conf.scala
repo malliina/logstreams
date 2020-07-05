d274017cf02843305c7e0890a6137108fd5ec03b
Michael Skogberg <malliina123@gmail.com>
2020-07-05 19:02:10 +0300

    Refac conf

--- server/app/com/malliina/logstreams/db/Conf.scala
+++ server/app/com/malliina/logstreams/db/Conf.scala
@@ -5,26 +5,19 @@ import play.api.Configuration
 case class Conf(url: String, user: String, pass: String, driver: String)
 
 object Conf {
-  val UrlKey = "logstreams.db.url"
-  val UserKey = "logstreams.db.user"
-  val PassKey = "logstreams.db.pass"
-  val DriverKey = "logstreams.db.driver"
   val MySQLDriver = "com.mysql.jdbc.Driver"
   val DefaultDriver = MySQLDriver
 
-  def fromEnvOrFail() = fromEnv().fold(err => throw new Exception(err), identity)
-  def fromConf(conf: Configuration) = from(key => conf.getOptional[String](key))
-  def fromEnv() = from(key => sys.env.get(key).orElse(sys.props.get(key)))
+  def fromConf(conf: Configuration) = from(conf.get[Configuration]("logstreams.db"))
 
-  def from(readKey: String => Option[String]) = {
-    def read(key: String) =
-      readKey(key)
-        .toRight(s"Key missing: '$key'.")
+  def from(conf: Configuration) = {
+    def read(key: String): Either[String, String] =
+      conf.getOptional[String](key).toRight(s"Key missing: '$key'.")
 
     for {
-      url <- read(UrlKey)
-      user <- read(UserKey)
-      pass <- read(PassKey)
-    } yield Conf(url, user, pass, read(DriverKey).getOrElse(DefaultDriver))
+      url <- read("url")
+      user <- read("user")
+      pass <- read("pass")
+    } yield Conf(url, user, pass, read("driver").getOrElse(DefaultDriver))
   }
 }
